086d609fca42180e7bcabfd04b2f03bce1399fec
vishrutss <vishrutss@gmail.com>
2023-10-27 22:16:31 -0700

    Implemented game loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,35 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
     private GameWindow window;
     private GamePanel panel;
+    private Thread gameThread;
+    private final int FPS_LIMIT = 120;
 
     public Game() {
         panel = new GamePanel();
         window = new GameWindow(panel);
         panel.requestFocus();
+        startGameLoop();
 
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1_000_000_000.0 / FPS_LIMIT;
+        long currentFrame = System.nanoTime(), lastFrame = System.nanoTime();
+
+        while (true) {
+            currentFrame = System.nanoTime();
+            if (currentFrame - lastFrame >= timePerFrame) {
+                panel.repaint();
+                lastFrame = currentFrame;
+            }
+        }
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,12 +5,17 @@ import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDirection = 0.1f, yDirection = 0.1f;
+    private int fps = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(123, 50, 250);
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
@@ -19,7 +24,7 @@ public class GamePanel extends JPanel {
         addMouseMotionListener(mouseInputs);
     }
 
-    public void setRectPosition(int x, int y){
+    public void setRectPosition(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
         repaint();
@@ -37,6 +42,29 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(xDelta, yDelta, 50, 50);
+
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta, 50, 50);
+
+        fps++;
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + fps);
+            fps = 0;
+        }
+    }
+
+    private void updateRectangle() {
+        xDelta += xDirection;
+        yDelta += yDirection;
+        if (xDelta > getWidth() || xDelta < 0) {
+            xDirection *= -1;
+            color= new Color((int)(Math.random() * 0x1000000));
+        }
+        if (yDelta > getHeight() || yDelta < 0) {
+            yDirection *= -1;
+            color= new Color((int)(Math.random() * 0x1000000));
+        }
     }
 }
